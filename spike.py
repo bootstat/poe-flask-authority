20a95488cc88a07919a9421165f766ecdf7f9193
bootstat <51894590+bootstat@users.noreply.github.com>
2019-06-19 05:53:07 -0500

    delete spike.py
    renamed to example.py

--- spike.py
+++
@@ -1,7 +0,0 @@
-from trade_api.poe_trade_interface import POETradeInterface
-
-interface = POETradeInterface()
-search_args = {"type" : ["Bow"], "pdps_min" : "300"}
-url = interface.get_query_url(search_args)
-print url
-interface.get_cheapest_query_results(url)
